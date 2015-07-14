03980513748e65bbc6e7ab4e611bd83cfee3e16d
Michael Stork <m.stork@recolize.com>
2015-07-14 19:05:11 +0200

    SUGRE-256: - added new license comments to all files

--- app/code/community/Recolize/RecommendationEngine/Model/Session.php
+++ app/code/community/Recolize/RecommendationEngine/Model/Session.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Recolize GmbH
+ *
+ * @section LICENSE
+ * This source file is subject to the GNU General Public License Version 3 (GPLv3).
+ *
+ * @category Recolize
+ * @package Recolize_RecommendationEngine
+ * @author Recolize GmbH <service@recolize.com>
+ * @copyright 2015 Recolize GmbH (http://www.recolize.com)
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License Version 3 (GPLv3).
+ */
 class Recolize_RecommendationEngine_Model_Session extends Mage_Core_Model_Session_Abstract
 {
     /**
--- app/code/community/Recolize/RecommendationEngine/sql/recolize_recommendation_engine_setup/mysql4-install-2.0.0.php
+++ app/code/community/Recolize/RecommendationEngine/sql/recolize_recommendation_engine_setup/mysql4-install-2.0.0.php
@@ -1,5 +1,16 @@
 <?php
-
+/**
+ * Recolize GmbH
+ *
+ * @section LICENSE
+ * This source file is subject to the GNU General Public License Version 3 (GPLv3).
+ *
+ * @category Recolize
+ * @package Recolize_RecommendationEngine
+ * @author Recolize GmbH <service@recolize.com>
+ * @copyright 2015 Recolize GmbH (http://www.recolize.com)
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License Version 3 (GPLv3).
+ */
 /** @var $installer Mage_Core_Model_Resource_Setup */
 $installer = $this;
 
